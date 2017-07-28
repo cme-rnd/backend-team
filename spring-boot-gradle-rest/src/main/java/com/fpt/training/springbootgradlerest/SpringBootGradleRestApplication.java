ad19b90c0ac1d288236aaf0426ef47836e6d85b0
HieuNT48 <hieunt48@fsoft.com.vn>
2017-07-28 15:02:07 +0700

    Init spring boot gradle

---
+++ spring-boot-gradle-rest/src/main/java/com/fpt/training/springbootgradlerest/SpringBootGradleRestApplication.java
@@ -0,0 +1,12 @@
+package com.fpt.training.springbootgradlerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleRestApplication.class, args);
+	}
+}
